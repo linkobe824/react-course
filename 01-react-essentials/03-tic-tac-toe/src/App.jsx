dc49e0617595a0396c524d6457ddedadb10a6322
Manuel Beltran <manueldbeltranc@gmail.com>
2023-11-22 17:18:53 -0700

    remove the activePlayer state as it is redundat because it can be computed from another state

--- 01-react-essentials/03-tic-tac-toe/src/App.jsx
+++ 01-react-essentials/03-tic-tac-toe/src/App.jsx
@@ -4,18 +4,24 @@ import Player from './components/Player'
 import GameBoard from './components/GameBoard'
 import Log from './components/Log'
 
+function deriveActivePlayer (gameTurns) {
+  let currentPlayer = 'X'
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') currentPlayer = 'O'
+
+  return currentPlayer
+}
+
 function App () {
-  const [activePlayer, setActivePlayer] = useState('X')
+  // const [activePlayer, setActivePlayer] = useState('X') ya no se necesita para rerenderizar - no extra state
   const [gameTurns, setGameTurns] = useState([])
 
-  function handleSelectSquare (rowIndex, colIndex) {
-    setActivePlayer(curActivePlayer => curActivePlayer === 'X' ? 'O' : 'X')
-    setGameTurns(prevTurns => {
-      let currentPlayer = 'X'
+  const activePlayer = deriveActivePlayer(gameTurns)
 
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O'
-      }
+  function handleSelectSquare (rowIndex, colIndex) {
+    // setActivePlayer(curActivePlayer => curActivePlayer === 'X' ? 'O' : 'X')
+    setGameTurns(prevTurns => {
+      const currentPlayer = deriveActivePlayer(prevTurns)
       const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns]
       return updatedTurns
     })
